807db41d20e44fcabfe9b7c9471d0298a94cd4e6
ARUN-PRAKASH-95 <arunprakash.fns@gmail.com>
2019-04-05 20:35:58 +0200

    Typed one comment

--- fibonacci.py
+++ fibonacci.py
@@ -3,7 +3,7 @@ n = int(input('Enter the no of fibonacci terms you want: '))
 def fibonacci(n):
     a = 0
     b = 1
-    print(a)
+    print(a)  #print the first number
 
     for i in range(n-1):
         a = a+b

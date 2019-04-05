5be0d0b53e7de37dc330a92d33a923f1110e3f59
ARUN-PRAKASH-95 <arunprakash.fns@gmail.com>
2019-04-05 20:19:18 +0200

    First git program

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+n = int(input('Enter the no of fibonacci terms you want: '))
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    print(a)
+
+    for i in range(n-1):
+        a = a+b
+        b = a-b
+        if a % 2 == 0 :
+            print(a)
+fibonacci(n)
